d624850b04b80244c08490627033eeeac8df3198
grtswt <grtswt@hotmail.com>
2015-04-27 22:02:13 +0200

    Add check_mk plugin

--- web/plugins/wato/gmap.py
+++
@@ -1,9 +0,0 @@
-declare_host_attribute(
-	NagiosTextAttribute(
-		"_position", 
-		"_position", 
-		"GoogleMaps coordinates",
-                "Coordinates formatted like: 60.957679, 14.593085"),
-	show_in_table = True,
-        show_in_form = True,                 
-	show_in_folder = True)
